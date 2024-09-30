a04367244f92c37a5a8d1d337376bc80bd0a7ec6
Sara A. W <saraakwatanabe@gmail.com>
2024-09-30 19:54:12 -0300

    Refactor instance types in eks.tf to use t3.medium for EKS Managed Node Group(s)

--- eks.tf
+++ eks.tf
@@ -24,7 +24,7 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
-    instance_types = ["t3.micro"]
+    instance_types = ["t3.medium"]
 
     attach_cluster_primary_security_group = false
   }
@@ -36,7 +36,7 @@ module "eks" {
       max_size     = 1
       desired_size = 1
 
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.medium"]
       capacity_type  = "SPOT"
 
       tags = {
